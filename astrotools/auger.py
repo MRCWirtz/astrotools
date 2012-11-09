3a9e2e91eb21d21dd70626fc00dc624d5527c5b4
David Walz <walz@physik.rwth-aachen.de>
2012-11-09 17:05:05 +0100

    improved documentation for auger.plotSpectrum

--- astrotools/auger.py
+++ astrotools/auger.py
@@ -7,13 +7,11 @@ lE_auger = array([18.05, 18.15, 18.25, 18.35, 18.45, 18.55, 18.65, 18.75, 18.85,
 J_auger = array([3.03646e-17, 1.55187e-17, 6.9384e-18, 3.20104e-18, 1.52781e-18, 7.21632e-19, 3.62670e-19, 1.88793e-19, 1.01309e-19, 5.85637e-20, 3.27556e-20, 1.64764e-20, 8.27115e-21, 4.74636e-21, 2.13542e-21, 9.40782e-22, 3.18048e-22, 1.62447e-22, 6.92891e-23, 6.25398e-24, 3.20445e-24, 1.22847e-24, 0, 0, 0])
 Jup_auger = array([1.16824e-18, 6.14107e-19, 3.08672e-19, 1.64533e-19, 9.72578e-21, 6.04038e-21, 3.91761e-21, 2.52816e-21, 1.66645e-21, 1.13438e-21, 7.56617e-22, 4.87776e-22, 3.01319e-22, 2.05174e-22, 1.24329e-22, 7.16586e-23, 3.77454e-23, 2.29493e-23, 1.30299e-23, 4.78027e-24, 3.59431e-24, 2.14480e-24, 1.19737e-24, 3.24772e-25, 8.79582e-26])
 Jlo_auger = array([1.16824e-18, 6.14107e-19, 3.08672e-19, 1.64533e-19, 9.72578e-21, 6.04038e-21, 3.91761e-21, 2.52816e-21, 1.66645e-21, 1.13438e-21, 7.56617e-22, 4.87776e-22, 3.01319e-22, 2.05174e-22, 1.24329e-22, 7.16586e-23, 3.77454e-23, 2.29493e-23, 1.30299e-23, 3.92999e-24, 2.00836e-24, 7.69927e-25, 0, 0, 0])
-
 # scale with E^3
 c = (10**lE_auger)**3
 J_auger *= c
 Jup_auger *= c
 Jlo_auger *= c
-
 
 
 def plotSpectrum(E, W=None, b=11, label='Simulation'):
@@ -31,6 +29,19 @@ def plotSpectrum(E, W=None, b=11, label='Simulation'):
       bin number for the fit
   label : string
       label for the legend
+  
+  Returns
+  -------
+  figure : a matplotlib figure
+  
+  Examples
+  --------
+  from astrotools import auger
+  from pylab import *
+  E = logspace(0, 2, 1000)
+  W = E**(-3)
+  fig = auger.plotSpectrum(E, None, 7)
+  show()
   """
   logEnergies = log10(E) + 18
 
@@ -52,11 +63,8 @@ def plotSpectrum(E, W=None, b=11, label='Simulation'):
   J *= c
   Jup *= c
   Jlo *= c
-  print J
 
   # scale to Auger spectrum in given bin
-  print J_auger[b]
-  print J[b]
   c = J_auger[b] / J[b]
   J *= c
   Jup *= c
