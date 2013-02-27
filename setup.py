e28362ad330ac6a819a2a113afc18c383d10b30e
Gero Müller <devnull@localhost>
2013-02-27 14:02:07 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='astrotools',
+      version='1.0',
+      description='Python Astro Tools',
+      author='DAvid Walz, Gero Müller',
+      author_email='walz@physik.rwth-aachen.de, gmueller@physik.rwth-aachen.de',
+      url='https://forge.physik.rwth-aachen.de/projects/astro-tools',
+      packages=['astrotools'],
+     )
