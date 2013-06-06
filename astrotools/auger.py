0cc83bbac7c6185595273f01447143973a10a807
David Walz <walz@physik.rwth-aachen.de>
2013-06-06 13:25:09 +0100

    updated gumbel xmax parametriezation

--- astrotools/auger.py
+++ astrotools/auger.py
@@ -12,32 +12,40 @@ dXmax = np.genfromtxt(os.path.join(cdir, 'auger_xmax13.txt'), delimiter=',', nam
 xmaxBins = np.r_[ dXmax['logElo'].copy(), dXmax['logEhi'][-1] ]
 
 # Values for <Xmax>, sigma(Xmax) parameterization, cf. arXiv:1301.6637 tables 1 and 2.
-# xmaxParams[model] = [X0, D, xi, delta], [p0, p1, p2, a0, a1, b]
+# Parameters for Epos LHC and QGSJet II-04 are from the ICRC '13 Proceedings by Eun-Joo Ahn.
+# xmaxParams[model] = (X0, D, xi, delta, p0, p1, p2, a0, a1, b)
 xmaxParams = {
-    'Epos 1.99' : ([809.7, 62.2,  0.78,  0.08], [3279,  -47, 228, -0.461, -0.0041, 0.059]),
-    'Sibyll 2.1': ([795.1, 57.7, -0.04, -0.04], [2785, -364, 152, -0.368, -0.0049, 0.039]),
-    'QGSJet 01' : ([774.2, 49.7, -0.30,  1.92], [3852, -274, 169, -0.451, -0.0020, 0.057]),
-    'QGSJet II' : ([781.8, 45.8, -1.13,  1.71], [3163, -237,  60, -0.386, -0.0006, 0.043]),
-    'Epos LHC' : ([806.1, 55.6, 0.15, 0.83], [3284, -260, 132, -0.462, -0.0008, 0.059]),
-    'QGSJet II-04' : ([790.4, 54.4, -0.31, 0.24], [3738, -375, -21, -0.397, 0.0008, 0.046])}
+    'QGSJet 01'    : (774.2, 49.7, -0.30,  1.92, 3852, -274, 169, -0.451, -0.0020, 0.057),
+    'QGSJet II'    : (781.8, 45.8, -1.13,  1.71, 3163, -237,  60, -0.386, -0.0006, 0.043),
+    'QGSJet II-04' : (790.4, 54.4, -0.31,  0.24, 3738, -375, -21, -0.397,  0.0008, 0.046),
+    'Sibyll 2.1'   : (795.1, 57.7, -0.04, -0.04, 2785, -364, 152, -0.368, -0.0049, 0.039),
+    'Epos 1.99'    : (809.7, 62.2,  0.78,  0.08, 3279,  -47, 228, -0.461, -0.0041, 0.059),
+    'Epos-LHC'     : (806.1, 55.6,  0.15,  0.83, 3284, -260, 132, -0.462, -0.0008, 0.059)}
 
-# Parameters for gumble - Xmax distribution, cf. GAP-2012-030
-# Epos 1.99 values are from M. Domenico (private communication)
-# gumbelParams[model]['mu'] = [a0, a1, a2], [b0, b1, b2]
+# Parameters for gumble - Xmax distribution, cf. ARXIV:1305.2331 table 1.
+# gumbelParams[model]['mu'] = ((a0, a1, a2), (b0, b1, b2), (c0, c1, c2))
 gumbelParams = {
-    'Epos 1.99' : {
-        'mu'     : [[779.737, -11.4095, -1.96729], [62.3138, -0.29405, 0.139223]],
-        'sigma'  : [[28.8526, 8.10448, -1.92435], [-0.0827813, -0.960622, 0.214701]],
-        'lambda' : [[0.537829, 0.52411, 0.0468013], [0.00941438, 0.0233692, 0.00999622]]},
-    'Sibyll 2.1': {
-        'mu'     : [[768.880, -15.026, -1.125], [59.910, -0.980, 0.145]],
-        'sigma'  : [[31.717, 1.335, -0.601], [-1.912, 0.007, 0.086]],
-        'lambda' : [[0.683, 0.278, 0.012], [0.008, 0.051, 0.003]]},
     'QGSJet II' : {
-        'mu'     : [[756.599, -10.322, -1.247], [50.998, -0.239, 0.246]],
-        'sigma'  : [[39.033, 7.452, -2.176], [4.390, -1.688, 0.170]],
-        'lambda' : [[0.857, 0.686, -0.040], [0.179, 0.076, -0.0130]]}
-    }
+        'mu'     : ((758.444, -10.692, -1.253), (48.892, 0.02, 0.179), (-2.346, 0.348, -0.086)),
+        'sigma'  : ((39.033, 7.452, -2.176), (4.390, -1.688, 0.170)),
+        'lambda' : ((0.857, 0.686, -0.040), (0.179, 0.076, -0.0130))},
+    'QGSJet II-04' : {
+        'mu'     : ((761.383, -11.719, -1.372), (57.344, -1.731, 0.309), (-0.355, 0.273, -0.137)),
+        'sigma'  : ((35.221, 12.335, -2.889), (0.307, -1.147, 0.271)),
+        'lambda' : ((0.673, 0.694, -0.007), (0.060, -0.019, 0.017))},
+    'Sibyll 2.1' : {
+        'mu'     : ((770.104, -15.873, -0.960), (58.668, -0.124, -0.023), (-1.423, 0.977, -0.191)),
+        'sigma'  : ((31.717, 1.335, -0.601), (-1.912, 0.007, 0.086)),
+        'lambda' : ((0.683, 0.278, 0.012), (0.008, 0.051, 0.003))},
+    'Epos 1.99' : {
+        'mu'     : ((780.013, -11.488, -1.906), (61.911, -0.098, 0.038), (-0.405, 0.163, -0.095)),
+        'sigma'  : ((28.853, 8.104, -1.924), (-0.083, -0.961, 0.215)),
+        'lambda' : ((0.538, 0.524, 0.047), (0.009, 0.023, 0.010))},
+    'Epos-LHC' : {
+        'mu'     : ((775.589, -7.047, -2.427), (57.589, -0.743, 0.214), (-0.820, -0.169, -0.027)),
+        'sigma'  : ((29.403, 13.553, -3.154), (0.096, -0.961, 0.150)),
+        'lambda' : ((0.563, 0.711, 0.058), (0.039, 0.067, -0.004))}}
+
 # --------------------- DATA -------------------------
 
 
@@ -69,42 +77,52 @@ def randDeclination(n=1):
         raise Exception("randDeclination: stochastic failure")
     return dec[accept][:n]
 
-def randXmax(E, A, model='Epos 1.99'):
+def randXmax(E, A, model='Epos-LHC'):
     """
-    Random Xmax values for given energy E [EeV] and mass number A (cf. GAP-2012-030).
+    Random Xmax values for given energy E [EeV] and mass number A (cf. ARXIV:1305.2331).
     """
     lE = np.log10(E/10.)
     lnA = np.log(A)
-    D = np.array([np.ones(np.shape(E)), lnA, lnA**2])
+    D = np.array([np.ones(np.shape(A)), lnA, lnA**2])
 
     p = gumbelParams[model]
-    a, b = p['mu']
-    mu = np.dot(a, D) + np.dot(b, D) * lE
+
+    a, b, c = p['mu']
+    p0 = np.dot(a, D)
+    p1 = np.dot(b, D)
+    p2 = np.dot(c, D)
+    mu = p0 + p1*lE + p2*lE**2
+
     a, b = p['sigma']
-    sigma = np.dot(a, D) + np.dot(b, D) * lE
+    p0 = np.dot(a,D)
+    p1 = np.dot(b,D)
+    sigma = p0 + p1*lE
+
     a, b = p['lambda']
-    lambd = np.dot(a, D) + np.dot(b, D) * lE
+    p0 = np.dot(a,D)
+    p1 = np.dot(b,D)
+    lambd = p0 + p1*lE
 
     # cf. Kragujevac J. Math. 25 (2003) 19-29, theorem 3.1:
     # Y = -ln X is generalized Gumbel distributed for Erlang distributed X
     # Erlang is a special case of the gamma distribution
     return mu - sigma * np.log( np.random.gamma(lambd, 1./lambd) )
 
-def meanXmax(E, A, model='Epos 1.99'):
+def meanXmax(E, A, model='Epos-LHC'):
     """
     <Xmax> values for given energies E [EeV], mass numbers A and a hadronic interaction model.
     See arXiv:1301.6637
     """
-    X0, D, xi, delta = xmaxParams[model][0]
+    X0, D, xi, delta = xmaxParams[model][:4]
     lE = np.log10(E) - 1
     return X0 + D*lE + (xi - D/np.log(10) + delta*lE)*np.log(A)
 
-def varXmax(E, A, model='Epos 1.99'):
+def varXmax(E, A, model='Epos-LHC'):
     """
     Shower to shower fluctuations sigma^2_sh(Xmax) values for given energies E [EeV], mass numbers A and a hadronic interaction model.
     See arXiv:1301.6637
     """
-    p0, p1, p2, a0, a1, b = xmaxParams[model][1]
+    p0, p1, p2, a0, a1, b = xmaxParams[model][4:]
     lE = np.log10(E) - 1
     lnA = np.log(A)
     s2p = p0 + p1*lE + p2*(lE**2)
@@ -133,7 +151,7 @@ def lnADistribution(E, A, weights=None, bins=xmaxBins):
     mlnA, vlnA = stat.binnedMeanAndVariance(lE, np.log(A), bins, weights)
     return (lEc, mlnA, vlnA)
 
-def lnA2XmaxDistribution(lEc, mlnA, vlnA, model='Epos 1.99'):
+def lnA2XmaxDistribution(lEc, mlnA, vlnA, model='Epos-LHC'):
     """
     Convert an energy binned <lnA>, sigma^2(lnA) distribution to <Xmax>, sigma^2(Xmax), cf. arXiv:1301.6637
     
@@ -150,7 +168,7 @@ def lnA2XmaxDistribution(lEc, mlnA, vlnA, model='Epos 1.99'):
     vXmax : Array of sigma^2(Xmax) in the energy bins of lEc
     """
     lEc = lEc - 19 # energy bin centers in log10(E / 10 EeV)
-    [X0, D, xi, delta], [p0, p1, p2, a0, a1, b] = xmaxParams[model]
+    X0, D, xi, delta, p0, p1, p2, a0, a1, b = xmaxParams[model]
 
     fE = (xi - D/np.log(10) + delta*lEc)
     s2p = p0 + p1*lEc + p2*(lEc**2)
@@ -160,7 +178,7 @@ def lnA2XmaxDistribution(lEc, mlnA, vlnA, model='Epos 1.99'):
     vXmax = s2p*( 1 + a*mlnA + b*(vlnA + mlnA**2) ) + fE**2*vlnA # eq. 2.12
     return (mXmax, vXmax)
 
-def xmaxDistribution(E, A, weights=None, model='Epos 1.99', bins=xmaxBins):
+def xmaxDistribution(E, A, weights=None, model='Epos-LHC', bins=xmaxBins):
     """
     Energy binned <Xmax>, sigma^2(Xmax), cf. arXiv:1301.6637
 
@@ -188,8 +206,8 @@ def spectrum(E, weights=None, normalize2bin=None):
     Optionally normalize to Auger spectrum in given bin.
     """
     lEshift = np.log10(1.14) # systematic upshift by 14%
-    bins = np.linspace(18.0, 20.5, 26)# + lEshift
-    N, bins = np.histogram(np.log10(E) + 18, bins, weights=weights)
+    bins = np.linspace(18.0, 20.5, 26) + lEshift
+    N, bins = np.histogram(np.log10(E) +18, bins, weights=weights)
     binWidths = 10**bins[1:] - 10**bins[:-1] # linear bin widths
     J = N / binWidths # make differential
     if normalize2bin:
@@ -207,7 +225,7 @@ def spectrumGroups(E, A, weights=None, normalize2bin=None):
     # spectrum (non-differential) with same bins as the Auger spectrum
     lE = np.log10(E) + 18
     lEshift = np.log10(1.14)
-    lEbins = np.linspace(18, 20.5, 26)# + lEshift
+    lEbins = np.linspace(18, 20.5, 26) + lEshift
     N = np.histogram(lE, weights=weights, bins=lEbins)[0]
 
     if weights == None:
@@ -242,7 +260,7 @@ def plotSpectrum(yList=None):
     """
     lEshift = np.log10(1.14) # systematic upshift by 14%
 
-    logE = dSpectrum['logE']# + lEshift
+    logE = dSpectrum['logE'] + lEshift
     c = (10**logE)**3 # scale with E^3
     J = c * dSpectrum['mean']
     Jhi = c * dSpectrum['stathi']
