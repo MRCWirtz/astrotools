1f0792d32b622ac202b6e5a420eb61abe6b4bcb6
MarCus <devnull@localhost>
2017-05-02 17:49:08 -0700

    import bug in testcase fixed.

--- test/test_simulations.py
+++ test/test_simulations.py
@@ -1,7 +1,7 @@
 import unittest
 
 import numpy as np
-from simulations import CosmicRaySimulation
+from astrotools.simulations import CosmicRaySimulation
 from astrotools import gamale
 
 __author__ = 'Marcus Wirtz'
