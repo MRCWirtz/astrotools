146d42cacfd9be6889ea6e18fca512293ab2d788
marcus <marcus@marcus-latitude>
2017-10-27 16:32:52 +0200

    - Unittest for auger.py implemented

--- test/test_auger.py
+++ test/test_auger.py
@@ -1,45 +1,110 @@
+import unittest
 from astrotools import auger
-from pylab import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+nlog10e = 20
+stat = 50000
+log10e_bins = np.linspace(18, 20, nlog10e + 1)
+log10e_cens = (log10e_bins[1:] + log10e_bins[:-1]) / 2
+A = np.ones(nlog10e)
+plots = False
 
 
-nE = 20
-lEbins = linspace(18, 20, nE+1)
-lEcens = (lEbins[1:]+lEbins[:-1])/2
-E = 10**(lEcens-18)
-A = ones(nE)
+def plotting(m_x2, s_x2, model):
+
+    l_e, m_x1, v_x1 = auger.xmax_moments(log10e_cens, A, bins=log10e_bins, model=model)
+
+    if not plots:
+        return
+
+    fix, ax = plt.subplots(2, 1, figsize=(14, 12))
+    ax1, ax2 = ax[0], ax[1]
+    ax1.plot(log10e_cens, m_x1, label='lnA -> Xmax')
+    ax1.plot(log10e_cens, m_x2, label='gumbel -> Xmax')
+    ax1.legend(loc='upper left', frameon=False)
+    ax1.set_xlabel('$\log_{10}$(E/[eV])')
+    ax1.set_ylabel(r'$\langle \rm{X_{max}} \rangle $ [g cm$^{-2}$]')
+
+    ax2.plot(log10e_cens, v_x1 ** .5, label='lnA -> Xmax')
+    ax2.plot(log10e_cens, s_x2, label='gumbel -> Xmax')
+    ax2.legend(loc='upper left', frameon=False)
+    ax2.set_xlabel('$\log_{10}$(E/[eV])')
+    ax2.set_ylabel(r'$\sigma(\rm{X_{max}})$ [g cm$^{-2}$]')
+
+    plt.suptitle(model)
+    plt.tight_layout()
+    plt.savefig('xmax_lna_%s.png' % model)
+    plt.clf()
 
 
-def compare(model):
-    # TODO fix test as xmax_distribution does not exist anymore
-    lE, mX1, vX1 = auger.xmaxDistribution(E, A, bins=lEbins, model=model)
+def moments(model):
 
-    mX2 = zeros(nE)
-    sX2 = zeros(nE)
-    for i in range(nE):
-        X = auger.rand_gumbel(ones(100000)*E[i], ones(100000)*A[i], model=model)
-        mX2[i] = mean(X)
-        sX2[i] = std(X)
+    m_x2 = np.zeros(nlog10e)
+    s_x2 = np.zeros(nlog10e)
+    for i in range(nlog10e):
+        x_gumbel = auger.rand_gumbel(np.ones(stat) * log10e_cens[i], np.ones(stat) * A[i], model=model)
+        m_x2[i] = np.mean(x_gumbel)
+        s_x2[i] = np.std(x_gumbel)
 
-    figure(figsize=(12, 6))
-    subplot(121)
-    plot(lEcens, mX1)
-    plot(lEcens, mX2, label='gumbel')
-    legend(loc='upper left', frameon=False)
-    xlabel('$\log_{10}$(E/[eV])')
-    ylabel(r'$\langle \rm{X_{max}} \rangle $ [g cm$^{-2}$]')
+    return m_x2, s_x2
 
-    subplot(122)
-    plot(lEcens, vX1**.5)
-    plot(lEcens, sX2)
-    xlabel('$\log_{10}$(E/[eV])')
-    ylabel(r'$\sigma(\rm{X_{max}})$ [g cm$^{-2}$]')
 
-    suptitle(model)
-    tight_layout()
+class TestXmaxlNA(unittest.TestCase):
 
-compare('Epos 1.99')
-compare('Epos-LHC')
-compare('Sibyll 2.1')
-compare('QGSJet II-04')
-compare('QGSJet II')
-show()
+    def test_01_epos_lhc(self):
+
+        model = 'EPOS-LHC'
+        m_x2, s_x2 = moments(model)
+        plotting(m_x2, s_x2, model)
+
+        self.assertTrue(True)
+
+    def test_02_sybill(self):
+
+        model = 'Sibyll2.1'
+        m_x2, s_x2 = moments(model)
+        plotting(m_x2, s_x2, model)
+
+        self.assertTrue(True)
+
+    def test_03_qgs2(self):
+
+        model = 'QGSJetII'
+        m_x2, s_x2 = moments(model)
+        plotting(m_x2, s_x2, model)
+
+        self.assertTrue(True)
+
+    def test_04_qgs4(self):
+
+        model = 'QGSJetII-04'
+        m_x2, s_x2 = moments(model)
+        plotting(m_x2, s_x2, model)
+
+        self.assertTrue(True)
+
+
+class EnergyAndCharge(unittest.TestCase):
+
+    def test_01_composition(self):
+
+        n = 1000
+        log10e = 18.5 + np.random.random(n)
+        charge = auger.rand_charge_from_auger(log10e)
+
+        self.assertTrue(charge.size == n)
+        self.assertTrue((charge >= 1).all() & (charge <= 26).all())
+
+    def test_02_energy(self):
+
+        n = 1000
+        log10e_min = 19.
+        log10e = auger.rand_energy_from_auger(n, log10e_min)
+        self.assertTrue(log10e.size == n)
+        self.assertTrue((log10e >= log10e_min).all() & (log10e <= 20.5).all())
+        self.assertTrue(len(log10e[log10e > log10e_min + 0.1]) < n)
+
+
+if __name__ == '__main__':
+    unittest.main()
