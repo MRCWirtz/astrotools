67516b100d55dfc7aba0cc836840c117d9b49c2f
MRCWirtz <marcus.wirtz@rwth-aachen.de>
2018-10-24 15:55:43 +0200

    Change version number to 1.3.0

--- setup.py
+++ setup.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='astrotools',
-    version='2.0',
+    version='1.3.0',
     description='Python Astro Tools',
-    author='Gero Müller, Martin Urban, David Walz, Marcus Wirtz',
-    author_email='gmueller@physik.rwth-aachen.de, murban@physik.rwth-aachen.de, walz@physik.rwth-aachen.de, '
-                 'mwirtz@physik.rwth-aachen.de',
-    url='https://forge.physik.rwth-aachen.de/projects/astrotools',
+    long_description="""The astrotools documentation is provided http://astro.pages.rwth-aachen.de/astrotools/""",
+    long_description_content_type='text/markdown',
+    author='Martin Urban, David Walz, Marcus Wirtz',
+    author_email='murban@physik.rwth-aachen.de, walz@physik.rwth-aachen.de, mwirtz@physik.rwth-aachen.de',
+    license='MIT',
+    keywords='astro auger',
+    url='http://astro.pages.rwth-aachen.de/astrotools/',
+    project_urls={
+        'Documentation': 'http://astro.pages.rwth-aachen.de/astrotools/',
+        'Source': 'https://git.rwth-aachen.de/astro/astrotools',
+        'Tutorial': 'http://astro.pages.rwth-aachen.de/astrotools/tutorial.html'
+        },
     packages=['astrotools'],
     package_data={'astrotools': ['data/*.txt', 'data/lnA/*', 'data/xmax/*', 'data/comp/*']},
-    requires=['numpy', 'healpy', 'matplotlib', 'scipy']
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        ],
+    install_requires=['numpy', 'healpy', 'matplotlib', 'scipy']
 )
