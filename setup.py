5118ace256486556dcd5e049b442664bdf11562f
MRCWirtz <marcus.wirtz@rwth-aachen.de>
2019-09-25 11:23:16 +0200

    Change version number to 1.4.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='astrotools',
-    version='1.4.2',
+    version='1.4.3',
     description='Python Astro Tools',
     long_description="""The astrotools documentation is provided http://astro.pages.rwth-aachen.de/astrotools/""",
     long_description_content_type='text/markdown',
